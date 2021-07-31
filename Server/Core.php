abc65ff419b79c4d823fd0c415f4664499925c33
lolikonschik <vlad.chernovw@gmail.com>
2021-07-31 15:14:45 +0200

    add new 3

---
+++ Server/Core.php
@@ -0,0 +1,27 @@
+<?php
+
+include("ILib.php");
+$cmd = base64_decode($_GET['cmd']);//принимает из адрес строки, ловит и расшифровывает
+
+switch ($cmd)
+{
+	case 'GetLicense':
+		echo base64_encode(Get('App/Data', 'App', 'LIC'));//открывает App берем знач LIC
+		break;
+
+	case 'GetRun':
+		echo base64_encode(Get('App/Data', 'App', 'RUN'));
+		break;
+
+	case 'PutLicense':
+		Write('App/Data', 'App', 'LIC', Get('App/Data', 'App', 'LIC') + 1);
+		echo base64_encode(Get('App/Data', 'App', 'LIC'));
+		break;
+
+	case 'PutRun':
+		Write('App/Data', 'App', 'RUN', Get('App/Data', 'App', 'RUN') + 1);
+		echo base64_encode(Get('App/Data', 'App', 'RUN'));
+		break;
+}
+
+?>
